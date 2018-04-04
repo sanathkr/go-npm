b8ff14bbd68002c5490999d9df9465624576a288
Julian Møller <git@julianmoeller.dk>
2018-04-04 09:33:08 +0200

    feat: switch to agnostic command for finding bin folder

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const ARCH_MAPPING = {
     "arm": "arm"
 };
 
-// Mapping between Node's `process.platform` to Golang's 
+// Mapping between Node's `process.platform` to Golang's
 const PLATFORM_MAPPING = {
     "darwin": "darwin",
     "linux": "linux",
@@ -27,9 +27,10 @@ const PLATFORM_MAPPING = {
 
 function getInstallationPath(callback) {
 
-    // `npm bin` will output the path where binary files should be installed
-    exec("npm bin", function(err, stdout, stderr) {
-
+    // `$npm_execpath bin` will output the path where binary files should be installed
+    // using whichever package manager is current
+    const packageManager = process.env.npm_execpath || 'npm';
+    exec(`${packageManager} bin`, function(err, stdout, stderr) {
         let dir =  null;
         if (err || stderr || !stdout || stdout.length === 0)  {
 
